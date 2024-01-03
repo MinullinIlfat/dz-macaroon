dfb1e1cf1b514d74dda8301dbcf742a14bf08dba
Ilfat <redemax96@gmail.com>
2024-01-03 12:30:18 +0300

    fixed phone-number

--- src/app/pipes/phone.pipe.ts
+++ src/app/pipes/phone.pipe.ts
@@ -6,8 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PhonePipe implements PipeTransform {
 
   transform(value: string): string {
-    // regex = /([+][1-9]+[(][1-9]+[)][1-9]+)/gm;
-    // return value.replace(value, regex)
-      return value
+    return value.replace(new RegExp('[0-9]*'), '+375 (29) 3689868')
   }
 }
